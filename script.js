c2934bc6763797358b0f7454f420b8657304c8c6
Arthur <arthurramos7890@gmail.com>
2025-04-06 16:32:10 -0300

    alterações no estilo e funcionalidades

--- script.js
+++ script.js
@@ -1,46 +1,45 @@
 const newsletters = [
-    {
-      title: "Edição 01 - Boas-vindas (06/04)",
-      file: "./edicoes/00_04.md"
-    },
-    {
-      title: "Edição 02 - Novidades de Abril",
-      file: "./edicoes/01_04.md"
-    },
-    {
-      title: "Edição 03 - Testando mais um",
-      file: "./edicoes/02_04.md"
-    }
-  ];
-  
-  const listEl = document.getElementById("newsletter-list");
-  const viewEl = document.getElementById("newsletter-view");
-  const contentEl = document.getElementById("newsletter-content");
-  
-  function mostrarLista() {
-    viewEl.style.display = "none";
-    listEl.style.display = "block";
-  }
-  
-  function voltar() {
-    mostrarLista();
-  }
-  
-  function mostrarEdicao(newsletter) {
-    fetch(newsletter.file)
-      .then(res => res.text())
-      .then(md => {
-        contentEl.innerHTML = marked.parse(md);
-        listEl.style.display = "none";
-        viewEl.style.display = "block";
-      });
-  }
-  
-  // Gera lista de edições
-  newsletters.forEach(newsletter => {
-    const li = document.createElement("li");
-    li.textContent = newsletter.title;
-    li.onclick = () => mostrarEdicao(newsletter);
-    listEl.appendChild(li);
-  });
-  
+  {
+    title: "Edição 01 - Boas-vindas (06/04)",
+    file: "./edicoes/00_04.md",
+  },
+  {
+    title: "Edição 02 - Novidades de Abril",
+    file: "./edicoes/01_04.md",
+  },
+  {
+    title: "Edição 03 - Testando mais um",
+    file: "./edicoes/02_04.md",
+  },
+];
+
+const listEl = document.getElementById("newsletter-list");
+const viewEl = document.getElementById("newsletter-view");
+const contentEl = document.getElementById("newsletter-content");
+
+function mostrarLista() {
+  viewEl.style.display = "none";
+  listEl.style.display = "block";
+}
+
+function voltar() {
+  mostrarLista();
+}
+
+function mostrarEdicao(newsletter) {
+  fetch(newsletter.file)
+    .then((res) => res.text())
+    .then((md) => {
+      contentEl.innerHTML = marked.parse(md);
+      listEl.style.display = "none";
+      viewEl.style.display = "block";
+    });
+}
+
+// Gera lista de edições
+newsletters.forEach((newsletter) => {
+  const li = document.createElement("li");
+  li.textContent = newsletter.title;
+  li.onclick = () => mostrarEdicao(newsletter);
+  listEl.appendChild(li);
+});
