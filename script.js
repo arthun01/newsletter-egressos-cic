d658991444a7a3c7eba201ea09089168501cb6c3
arthur <arthurramos7890@gmail.com>
2025-05-16 15:21:58 -0300

    mudança de visual (listas para cards) responsividade com cards organizei o estilo (diferentes arquivos) criei um botão para mostrar os participantes mas ainda não botei nada lá

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 const newsletters = [
   {
-    title: "Edição 01 - Boas-vindas (06/04)",
+    title: "Edição 01 - Boas-vindas",
+    date: "06/04/2025",
     file: "./edicoes/00_04.md",
+    image: "./imagens/backs/bg1.png"
   },
   {
     title: "Edição 02 - Novidades de Abril",
+    date: "13/04/2025",
     file: "./edicoes/01_04.md",
+    image: "./imagens/backs/bg2.png"
   },
   {
     title: "Edição 03 - Testando mais um",
+    date: "20/04/2025",
     file: "./edicoes/02_04.md",
+    image: "./imagens/bg3.jpg"
   },
+  {
+    title: "Edição 04 - Testando mais um",
+    date: "23/04/2025",
+    file: "./edicoes/02_04.md",
+    image: "./imagens/bg3.jpg"
+  }
 ];
 
 const listEl = document.getElementById("newsletter-list");
@@ -19,7 +31,7 @@ const contentEl = document.getElementById("newsletter-content");
 
 function mostrarLista() {
   viewEl.style.display = "none";
-  listEl.style.display = "block";
+  listEl.style.display = "grid";
 }
 
 function voltar() {
@@ -36,10 +48,21 @@ function mostrarEdicao(newsletter) {
     });
 }
 
-// Gera lista de edições
+
 newsletters.forEach((newsletter) => {
   const li = document.createElement("li");
-  li.textContent = newsletter.title;
+  li.className = "card";
+  li.style.backgroundImage = `url(${newsletter.image})`;
+
+  li.innerHTML = `
+    <div class="card-content">
+      <h3>${newsletter.title}</h3>
+      <p>${newsletter.date}</p>
+    </div>
+  `;
+
   li.onclick = () => mostrarEdicao(newsletter);
   listEl.appendChild(li);
 });
+
+//Fazer uma limpa nesse código e deixar ele o mais sucinto e autoexplicativo possível
